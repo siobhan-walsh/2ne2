711e8dbb1ec4f56ac221472f0b13f03468a54c5b
chris <chris@10.64.22.5>
2017-09-23 17:45:14 -0700

    Merge

--- WebContent/js/admin.js
+++ WebContent/js/admin.js
@@ -1,4 +1,14 @@
 $(document).ready(function(e) {
 	console.log('put admin functions here');
 	//document.querySelector("#adminTest").addEventListener("click", changeBackground);
+	/*
+	function getData(){
+		$.ajax({
+		  method: "POST",
+		  url: "some.php",
+		  data: { name: "John", location: "Boston" }
+		})
+		
+	}
+	*/
 });
